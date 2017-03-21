53e185497c932acc5d40c1e58d6bc6847a089e5c
mcharlee <rm.charles.miller@gmail.com>
2017-03-21 01:24:59 -0600

    Practice Fragments/JSON
    This will switch fragments based on orientation and read JSON file into
    String

---
+++ app/src/main/java/com/uprog/eric/fragmentpractice/Blurp.java
@@ -0,0 +1,10 @@
+package com.uprog.eric.fragmentpractice;
+
+import org.json.JSONObject;
+
+/**
+ * Created by rm_ch on 3/15/2017.
+ */
+
+public class Blurp {
+}
---
+++ app/src/main/java/com/uprog/eric/fragmentpractice/MainActivity.java
@@ -0,0 +1,71 @@
+package com.uprog.eric.fragmentpractice;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.JsonReader;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+import java.io.*;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        String fileString = fileRead();
+
+        Configuration config = getResources().getConfiguration();
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if(config.orientation == Configuration.ORIENTATION_LANDSCAPE){
+            LM_Fragment lm_fragment = new LM_Fragment();
+            fragmentTransaction.replace(android.R.id.content, lm_fragment);
+        }
+        else{
+            PM_Fragment pm_fragment = new PM_Fragment();
+            fragmentTransaction.replace(android.R.id.content,pm_fragment);
+            pm_fragment.textView.setText(fileString.substring(0,10));
+        }
+        fragmentTransaction.commit();
+
+
+    }
+    public String fileRead(){
+        String ret = "";
+        InputStream is;
+        StringBuilder sb;
+        try {
+            is = getAssets().open("lds_scriptures.json");
+            if (is != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(is);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ((receiveString = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(receiveString);
+                }
+
+                is.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+}
